827c24fabc381f2ae1b421bf6e2b800642b8877e
RyanG <ryan.graham@gmail.com>
2014-10-27 23:47:07 +0000

    distutils setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'zerg',
+  packages = ['zerg'],
+  version = '0.1.1',
+  description = 'somewhat-parallel ssh',
+  author = 'Ryan Graham',
+  author_email = 'ryangraham@gmail.com',
+  url = 'https://github.com/ryangraham/zerg',
+  download_url = 'https://github.com/ryangraham/zerg/tarball/0.1.1',
+  keywords = ['ssh', 'remote', 'parallel'],
+  classifiers = [],
+)
